70f1285e7a69d767d9e0bd17683aae15b52e7223
Michael F. Herbst <info@michael-herbst.com>
2017-02-19 10:22:20 +0100

    Fix whitespace in setup_dependencies

--- cmake/setup_dependencies.cmake
+++ cmake/setup_dependencies.cmake
@@ -1,14 +1,14 @@
 # sets these things
 #
-# 	GSCF_DEPENDENCIES			everyone needs these libraries
-# 	GSCF_DEPENDENCIES_DEBUG		debug mode needs these extras
-# 	GSCF_DEPENDENCIES_RELEASE		release mode needs these extras
-# 	GSCF_DEPENDENCIES_TEST		tests need these extra libraries
+#       GSCF_DEPENDENCIES			everyone needs these libraries
+#       GSCF_DEPENDENCIES_DEBUG			debug mode needs these extras
+#       GSCF_DEPENDENCIES_RELEASE		release mode needs these extras
+#       GSCF_DEPENDENCIES_TEST			tests need these extra libraries
 #
 #       GSCF_DEFINITIONS			definitions for all compilation
-#       GSCF_DEFINITIONS_DEBUG		definitions for debug mode
+#       GSCF_DEFINITIONS_DEBUG			definitions for debug mode
 #       GSCF_DEFINITIONS_RELEASE		definitions for release mode
-#       
+#
 
 ####################
 #-- Empty it all --#

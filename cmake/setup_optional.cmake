4f95642460efa5b70c17c1707fedb6643499ba8f
Michael F. Herbst <info@michael-herbst.com>
2017-08-05 17:35:32 +0200

    Bugfix: Set variable instead of definitions

--- cmake/setup_optional.cmake
+++ cmake/setup_optional.cmake
@@ -22,10 +22,10 @@
 # Setup optional dependencies and features
 # alters these things
 #
-# 	GSCF_DEPENDENCIES			everyone needs these libraries
-# 	GSCF_DEPENDENCIES_DEBUG		debug mode needs these extras
-# 	GSCF_DEPENDENCIES_RELEASE		release mode needs these extras
-# 	GSCF_DEPENDENCIES_TEST		tests need these extra libraries
+#       GSCF_DEPENDENCIES			everyone needs these libraries
+#       GSCF_DEPENDENCIES_DEBUG		debug mode needs these extras
+#       GSCF_DEPENDENCIES_RELEASE		release mode needs these extras
+#       GSCF_DEPENDENCIES_TEST		tests need these extra libraries
 #
 #       GSCF_DEFINITIONS			definitions for all compilation
 #       GSCF_DEFINITIONS_DEBUG		definitions for debug mode
@@ -37,10 +37,10 @@
 ####################
 if (NOT CMAKE_CXX_STANDARD VERSION_LESS 14)
 	message(STATUS "Detected C++14 support: Setting GSCF_HAVE_CXX14")
-	LIST(APPEND GSCF_DEFINITIONS "GSCF_HAVE_CXX14")
+	set(GSCF_HAVE_CXX14 ON)
 endif()
 if (NOT CMAKE_CXX_STANDARD VERSION_LESS 17)
 	message(STATUS "Detected C++17 support: Setting GSCF_HAVE_CXX17")
-	LIST(APPEND GSCF_DEFINITIONS "GSCF_HAVE_CXX17")
+	set(GSCF_HAVE_CXX17 ON)
 endif()
 
